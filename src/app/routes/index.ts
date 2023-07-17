66f72d991908a01bbdee92177be931489a46322f
SaimaMeem <saima.sultana1320@gmail.com>
2023-07-17 07:57:21 +0600

    feat: add book route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import { AuthRoutes } from '../modules/auth/auth.route';
+import { BookRoutes } from '../modules/book/book.route';
 
 const router = express.Router();
 
 const moduleRoutes = [
   {
-    path: '/',
-    route: AuthRoutes,
+    path: '/books/',
+    route: BookRoutes,
   },
 ];
 
